622872d6a30b0f34b9e345e550b4c0d17b9a962c
Matthew Schranz <matthew.schranz@rbc.com>
2020-01-15 14:57:53 -0500

    API tweaks basic

--- books-example.py
+++ books-example.py
@@ -23,35 +23,20 @@ books = [
      'published': '1975'}
 ]
 
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/api/v1/resources/books', methods=['GET'])
 def api_all():
     return jsonify(books)
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
+@app.route('/api/v1/resources/books/<int:book_id>', methods=['GET'])
+def api_id(book_id):
     # Create an empty list for our results
     results = []
 
     # Loop through the data and match results that fit the requested ID.
     # IDs are unique, but other fields might return many results
     for book in books:
-        if book['id'] == id:
+        if book['id'] == book_id:
             results.append(book)
 
     # Use the jsonify function from Flask to convert our list of
